342b761311f1a85e86dec9ab2bfe0cdc1fd2a5d5
Luke Hsiao <lwhsiao@stanford.edu>
2018-02-07 23:15:58 -0800

    Remove CoreNLP installation
    Former-commit-id: ce17efb445acc64eb5ad2471dbc4f31e4965cb69

--- run.sh
+++ run.sh
@@ -7,17 +7,6 @@ cd "$FONDUERHOME"
 # Make sure the submodules are installed
 git submodule update --init --recursive
 
-# Make sure parser is installed
-PARSER="parser/stanford-corenlp-3.6.0.jar"
-if [ ! -f "$PARSER" ]; then
-    read -p "CoreNLP [default] parser not found- install now?  [y/n] " yn
-    case $yn in
-        [Yy]* ) echo "Installing parser..."; ./install-parser.sh;;
-        [Nn]* ) ;;
-    esac
-fi
-
-
 # Make sure phantomjs is installed
 PHANTOMJS="phantomjs/bin/phantomjs"
 if [ ! -f "$PHANTOMJS" ]; then
