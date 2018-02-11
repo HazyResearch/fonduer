8b7064e1d50314ab5fbd75cb665bee8d7680ce49
Luke Hsiao <lwhsiao@stanford.edu>
2018-02-10 22:30:48 -0800

    Prettify and fix bugs in visual
    Also remove the unused batch size in parser.py
    
    
    Former-commit-id: ec1ac4f1ba0aa529e877bd28aec78de35faefe47

--- fonduer/parser.py
+++ fonduer/parser.py
@@ -158,7 +158,6 @@ class OmniParserUDF(UDF):
             self.replacements.append((re.compile(pattern, flags=re.UNICODE),
                                       replace))
         if self.lingual:
-            self.batch_size = 7000  # character limit for CoreNLP, see issue #840
             self.lingual_parser = lingual_parser
             self.req_handler = lingual_parser.connect()
             self.lingual_parse = self.req_handler.parse
