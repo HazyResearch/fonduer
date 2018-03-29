a5ef253b7f76f99e186dfc25732d6df00da02a9c
Luke Hsiao <lwhsiao@stanford.edu>
2018-03-29 08:20:41 -0700

    Remove tutorials from this repo
    Splits the tutorials out to a separate repository located at
    https://github.com/HazyResearch/fonduer-tutorials, where they
    demonstrate how to use Fonduer as a Python package.
    
    Closes #23.

--- run.sh
+++
@@ -1,9 +0,0 @@
-# Set environment variables 
-source set_env.sh
-
-# Move to Fonduer home directory
-cd "$FONDUERHOME"
-
-# Launch jupyter notebook!
-echo "Launching Jupyter Notebook..."
-jupyter notebook --notebook-dir="$FONDUERHOME"
