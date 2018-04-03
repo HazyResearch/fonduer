c250d76298600cc6a18407e6be233fdadd117404
Luke Hsiao <lwhsiao@stanford.edu>
2018-04-03 10:31:16 -0700

    Fix imports

--- fonduer/models/__init__.py
+++ fonduer/models/__init__.py
@@ -1,13 +1,29 @@
+from fonduer.models.context import (Cell, Figure, FigureCaption, Header, Image,
+                                    ImplicitSpan, Para, Phrase, RefList,
+                                    Section, Table, TableCaption,
+                                    TemporaryImage, TemporaryImplicitSpan,
+                                    Webpage)
 from fonduer.snorkel.models.candidate import candidate_subclass
 from fonduer.snorkel.models.context import Document
 from fonduer.snorkel.models.meta import Meta
 
-from fonduer.models.context import (Webpage, Table, Cell, Phrase, Figure,
-                                    TemporaryImplicitSpan, ImplicitSpan,
-                                    TemporaryImage, Image)
-
 __all__ = [
-    'Document', 'Meta', 'candidate_subclass', 'Webpage', 'Table', 'Cell',
-    'Phrase', 'Figure', 'TemporaryImplicitSpan', 'ImplicitSpan',
-    'TemporaryImage', 'Image'
+    'Cell',
+    'Document',
+    'Figure',
+    'FigureCaption',
+    'Header',
+    'Image',
+    'ImplicitSpan',
+    'Meta',
+    'Para',
+    'Phrase',
+    'RefList',
+    'Section',
+    'Table',
+    'TableCaption',
+    'TemporaryImage',
+    'TemporaryImplicitSpan',
+    'Webpage',
+    'candidate_subclass',
 ]
