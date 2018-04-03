b396636c8e96af3197aeb6b53c57d75ad71761c4
Luke Hsiao <lwhsiao@stanford.edu>
2018-04-03 10:35:55 -0700

    Clean up import

--- fonduer/models/__init__.py
+++ fonduer/models/__init__.py
@@ -1,13 +1,21 @@
+from fonduer.models.context import (Cell, Figure, Image, ImplicitSpan, Phrase,
+                                    Table, TemporaryImage,
+                                    TemporaryImplicitSpan, Webpage)
 from fonduer.snorkel.models.candidate import candidate_subclass
 from fonduer.snorkel.models.context import Document
 from fonduer.snorkel.models.meta import Meta
 
-from fonduer.models.context import (Webpage, Table, Cell, Phrase, Figure,
-                                    TemporaryImplicitSpan, ImplicitSpan,
-                                    TemporaryImage, Image)
-
 __all__ = [
-    'Document', 'Meta', 'candidate_subclass', 'Webpage', 'Table', 'Cell',
-    'Phrase', 'Figure', 'TemporaryImplicitSpan', 'ImplicitSpan',
-    'TemporaryImage', 'Image'
+    'Cell',
+    'Document',
+    'Figure',
+    'Image',
+    'ImplicitSpan',
+    'Meta',
+    'Phrase',
+    'Table',
+    'TemporaryImage',
+    'TemporaryImplicitSpan',
+    'Webpage',
+    'candidate_subclass',
 ]
