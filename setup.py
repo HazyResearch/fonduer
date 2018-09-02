556c3b4819afc50ea0faf8887347bcdeee8a2e0c
Luke Hsiao <lwhsiao@stanford.edu>
2018-09-02 00:54:40 -0700

    Remove snorkel-metal version pinning

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "psycopg2-binary",
         "pyyaml",
         "scipy>=0.18",
-        "snorkel-metal==0.1.1",
+        "snorkel-metal",
         "spacy>=2.0.7",
         "sqlalchemy>=1.0.14",
         "torch>=0.4.0",
