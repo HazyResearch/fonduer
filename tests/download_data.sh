bc1418371573f335b919a27a214df888fec3bf30
Johannes Rausch <jrausch@inf.ethz.ch>
2018-09-04 10:52:39 -0700

    Undid temporary change in data download script

--- tests/download_data.sh
+++ tests/download_data.sh
@@ -1,8 +1,10 @@
 echo "Downloading fonduer test data..."
-#url=http://i.stanford.edu/hazy/share/fonduer/fonduer_test_data_v0.3.0.tar.gz
+url=http://i.stanford.edu/hazy/share/fonduer/fonduer_test_data_v0.3.0.tar.gz
 data_tar=fonduer_test_data_v0.3.0
-if type wget &>/dev/null; then
-    wget -v -O fonduer_test_data_v0.3.0.tar.gz -L https://stanford.box.com/shared/static/o9zdfdajkqx5cqwxt8cf8s1hwtcncmlm.gz
+if type curl &>/dev/null; then
+    curl -RLO $url
+elif type wget &>/dev/null; then
+    wget -N $url
 fi
 
 echo "Unpacking fonduer test data..."
