b3fb0128150496113a3373318aac590528713231
Luke Hsiao <lwhsiao@stanford.edu>
2018-09-12 14:29:52 -0700

    Add comments for Sentence
    This also fixes the MixIns to use @declared_attr, and removes the unused
    entity_cids and entity_types.

--- src/fonduer/parser/models/sentence.py
+++ src/fonduer/parser/models/sentence.py
@@ -35,13 +35,36 @@ class SentenceMixin(object):
 class LingualMixin(object):
     """A collection of lingual attributes."""
 
-    lemmas = Column(STR_ARRAY_TYPE)
-    pos_tags = Column(STR_ARRAY_TYPE)
-    ner_tags = Column(STR_ARRAY_TYPE)
-    dep_parents = Column(INT_ARRAY_TYPE)
-    dep_labels = Column(STR_ARRAY_TYPE)
+    @declared_attr
+    def lemmas(cls):
+        """A list of the lemmas for each word in a ``Sentence``."""
+        return Column(STR_ARRAY_TYPE)
+
+    @declared_attr
+    def pos_tags(cls):
+        """A list of POS tags for each word in a ``Sentence``."""
+        return Column(STR_ARRAY_TYPE)
+
+    @declared_attr
+    def ner_tags(cls):
+        """A list of NER tags for each word in a ``Sentence``."""
+        return Column(STR_ARRAY_TYPE)
+
+    @declared_attr
+    def dep_parents(cls):
+        """A list of the dependency parents for each word in a ``Sentence``."""
+        return Column(INT_ARRAY_TYPE)
+
+    @declared_attr
+    def dep_labels(cls):
+        """A list of dependency labels for each word in a ``Sentence``."""
+        return Column(STR_ARRAY_TYPE)
 
     def is_lingual(self):
+        """Whether or not the ``Sentence`` contains NLP information.
+
+        :rtype: bool
+        """
         return self.lemmas is not None
 
     def __repr__(self):
@@ -55,10 +78,12 @@ class TabularMixin(object):
 
     @declared_attr
     def table_id(cls):
+        """The id of the parent ``Table``, if any."""
         return Column("table_id", ForeignKey("table.id"))
 
     @declared_attr
     def table(cls):
+        """The parent ``Table``, if any."""
         return relationship(
             "Table",
             backref=backref("sentences", cascade="all, delete-orphan"),
@@ -67,26 +92,50 @@ class TabularMixin(object):
 
     @declared_attr
     def cell_id(cls):
+        """The id of the parent ``Cell``, if any."""
         return Column("cell_id", ForeignKey("cell.id"))
 
     @declared_attr
     def cell(cls):
+        """The parent ``Cell``, if any."""
         return relationship(
             "Cell",
             backref=backref("sentences", cascade="all, delete-orphan"),
             foreign_keys=lambda: cls.cell_id,
         )
 
-    row_start = Column(Integer)
-    row_end = Column(Integer)
-    col_start = Column(Integer)
-    col_end = Column(Integer)
-    position = Column(Integer)
+    @declared_attr
+    def row_start(cls):
+        """The ``row_start`` of the parent ``Cell``, if any."""
+        return Column(Integer)
+
+    @declared_attr
+    def row_end(cls):
+        """The ``row_end`` of the parent ``Cell``, if any."""
+        return Column(Integer)
+
+    @declared_attr
+    def col_start(cls):
+        """The ``col_start`` of the parent ``Cell``, if any."""
+        return Column(Integer)
+
+    @declared_attr
+    def col_end(cls):
+        """The ``col_end`` of the parent ``Cell``, if any."""
+        return Column(Integer)
 
     def is_tabular(self):
+        """Whether or not the ``Sentence`` contains tabular information.
+
+        :rtype: bool
+        """
         return self.table is not None
 
     def is_cellular(self):
+        """Whether or not the ``Sentence`` contains information about its table cell.
+
+        :rtype: bool
+        """
         return self.cell is not None
 
     def __repr__(self):
@@ -116,13 +165,39 @@ class TabularMixin(object):
 class VisualMixin(object):
     """A collection of visual attributes."""
 
-    page = Column(INT_ARRAY_TYPE)
-    top = Column(INT_ARRAY_TYPE)
-    left = Column(INT_ARRAY_TYPE)
-    bottom = Column(INT_ARRAY_TYPE)
-    right = Column(INT_ARRAY_TYPE)
+    @declared_attr
+    def page(cls):
+        """A list of the page index of each word in the ``Sentence``.
+
+        Page indexes start at 0.
+        """
+        return Column(INT_ARRAY_TYPE)
+
+    @declared_attr
+    def top(cls):
+        """A list of each word's TOP bounding box coordinate in the ``Sentence``."""
+        return Column(INT_ARRAY_TYPE)
+
+    @declared_attr
+    def left(cls):
+        """A list of each word's LEFT bounding box coordinate in the ``Sentence``."""
+        return Column(INT_ARRAY_TYPE)
+
+    @declared_attr
+    def bottom(cls):
+        """A list of each word's BOTTOM bounding box coordinate in the ``Sentence``."""
+        return Column(INT_ARRAY_TYPE)
+
+    @declared_attr
+    def right(cls):
+        """A list of each word's RIGHT bounding box coordinate in the ``Sentence``."""
+        return Column(INT_ARRAY_TYPE)
 
     def is_visual(self):
+        """Whether or not the ``Sentence`` contains visual information.
+
+        :rtype: bool
+        """
         return self.page is not None and self.page[0] is not None
 
     def __repr__(self):
@@ -142,11 +217,27 @@ class VisualMixin(object):
 class StructuralMixin(object):
     """A collection of structural attributes."""
 
-    xpath = Column(String)
-    html_tag = Column(String)
-    html_attrs = Column(STR_ARRAY_TYPE)
+    @declared_attr
+    def xpath(cls):
+        """The HTML XPATH to the ``Sentence``."""
+        return Column(String)
+
+    @declared_attr
+    def html_tag(cls):
+        """The HTML tag of the element containing the ``Sentence``."""
+        return Column(String)
+
+    #: The HTML attributes of the element the ``Sentence`` is found in.
+    @declared_attr
+    def html_attrs(cls):
+        """A list of the html attributes of the element containing the ``Sentence``."""
+        return Column(STR_ARRAY_TYPE)
 
     def is_structural(self):
+        """Whether or not the ``Sentence`` contains structural information.
+
+        :rtype: bool
+        """
         return self.html_tag is not None
 
     def __repr__(self):
@@ -163,31 +254,53 @@ class Sentence(
     """A Sentence subclass with Lingual, Tabular, Visual, and HTML attributes."""
 
     __tablename__ = "sentence"
+
+    #: The unique id for the ``Sentence``.
     id = Column(Integer, ForeignKey("context.id", ondelete="CASCADE"), primary_key=True)
+
+    #: The position of the ``Sentence`` in the ``Document``.
+    position = Column(Integer, nullable=False)  # unique sentence number per document
+
+    #: The id of the parent ``Document``.
     document_id = Column(Integer, ForeignKey("document.id"))
+
+    #: The the parent ``Document``.
     document = relationship(
         "Document",
         backref=backref("sentences", cascade="all, delete-orphan"),
         foreign_keys=document_id,
     )
+
+    #: The id of the parent ``Section``.
     section_id = Column(Integer, ForeignKey("section.id"))
+    #: The parent ``Section``.
     section = relationship(
         "Section",
         backref=backref("sentences", cascade="all, delete-orphan"),
         foreign_keys=section_id,
     )
+
+    #: The id of the parent ``Paragraph``.
     paragraph_id = Column(Integer, ForeignKey("paragraph.id"))
+    #: The parent ``Paragraph``.
     paragraph = relationship(
         "Paragraph",
         backref=backref("sentences", cascade="all, delete-orphan"),
         foreign_keys=paragraph_id,
     )
-    position = Column(Integer, nullable=False)  # unique sentence number per document
+
+    #: The full text of the ``Sentence``.
     text = Column(Text, nullable=False)
+
+    #: A list of the words in a ``Sentence``.
     words = Column(STR_ARRAY_TYPE)
+
+    #: A list of the character offsets of each word in a ``Sentence``, with
+    #: respect to the start of the sentence.
     char_offsets = Column(INT_ARRAY_TYPE)
-    entity_cids = Column(STR_ARRAY_TYPE)
-    entity_types = Column(STR_ARRAY_TYPE)
+
+    #: A list of the character offsets of each word in a ``Sentence``, with
+    #: respect to the entire document.
     abs_char_offsets = Column(INT_ARRAY_TYPE)
 
     __mapper_args__ = {"polymorphic_identity": "sentence"}
