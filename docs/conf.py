577be6d96cf2ec55e8e79691d37beadd4339f4c2
Luke Hsiao <lwhsiao@stanford.edu>
2018-09-18 14:26:30 -0700

    Add custom logo for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -103,6 +103,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["static"]
+html_logo = "static/img/fonduer.svg"
+html_theme_options = {"logo_only": True, "display_version": False}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
