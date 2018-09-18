4665d4e8c2ee00445ba9bedc8c00bfcaa713912e
Luke Hsiao <lwhsiao@stanford.edu>
2018-09-18 13:44:40 -0700

    Remove wrappers for metal functions
    We want the separation of responsibilities to be clear so that the user
    is fully aware of what parts of the code are owned by MeTaL.

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -5,6 +5,7 @@ import pickle
 
 import numpy as np
 import pytest
+from metal.label_model import LabelModel
 
 from fonduer import Meta
 from fonduer.candidates import CandidateExtractor, MentionExtractor
@@ -15,7 +16,7 @@ from fonduer.learning import LSTM, LogisticRegression, SparseLogisticRegression
 from fonduer.parser import Parser
 from fonduer.parser.models import Document, Sentence
 from fonduer.parser.preprocessors import HTMLDocPreprocessor
-from fonduer.supervision import Labeler, LabelLearner
+from fonduer.supervision import Labeler
 from fonduer.supervision.models import GoldLabel, Label, LabelKey
 from tests.shared.hardware_lfs import (
     LF_collector_aligned,
@@ -424,7 +425,7 @@ def test_e2e(caplog):
     L_train_gold = labeler.get_gold_labels(train_cands, annotator="gold")
     assert L_train_gold[0].shape == (3346, 1)
 
-    gen_model = LabelLearner(cardinalities=2)
+    gen_model = LabelModel(k=2)
     gen_model.train(L_train[0], n_epochs=500, print_every=100)
 
     train_marginals = gen_model.predict_proba(L_train[0])[:, 1]
@@ -478,7 +479,7 @@ def test_e2e(caplog):
     L_train = labeler.get_label_matrices(train_cands)
     assert L_train[0].shape == (3346, 13)
 
-    gen_model = LabelLearner(cardinalities=2)
+    gen_model = LabelModel(k=2)
     gen_model.train(L_train[0], n_epochs=500, print_every=100)
 
     train_marginals = gen_model.predict_proba(L_train[0])[:, 1]
