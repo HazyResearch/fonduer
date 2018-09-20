8314fc9fec70224a6b16511c0e15be5afb87ac93
Sen Wu <senwu@cs.stanford.edu>
2018-09-20 13:10:09 -0700

    update cand test

--- tests/candidates/test_candidates.py
+++ tests/candidates/test_candidates.py
@@ -90,8 +90,6 @@ def test_ngram_split(caplog):
     assert "A-B/C" in spans
     assert "B/C-D" in spans
     assert "A-B" in spans
-    assert "A-B/C" in spans
-    assert "B/C-D" in spans
     assert "C-D" in spans
     assert "B/C" in spans
     assert "A" in spans
