f4eef68a65b9fff04b397ea15808a6e12ad4cb5e
senwu <senwu@stanford.edu>
2018-12-09 11:22:35 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     ],
     extras_require={
         "spacy_ja": ["mecab-python3>=0.7,<0.8"],
-        "spacy_zh": ["jieba>=0.39, <0.4"],
+        "spacy_zh": ["jieba>=0.39, <0.40"],
     },
     keywords=["fonduer", "knowledge base construction", "richly formatted data"],
     include_package_data=True,
