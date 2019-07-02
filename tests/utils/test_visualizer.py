4b497f628f61caa2afa3e75f05ccc1d206bcf5d2
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2019-07-02 11:48:38 -0700

    Import get_pdf_dim only on Linux

--- tests/utils/test_visualizer.py
+++ tests/utils/test_visualizer.py
@@ -2,14 +2,14 @@ import sys
 
 import pytest
 
-from fonduer.utils.visualizer import get_pdf_dim
-
 
 # TODO test on MacOSX too
 @pytest.mark.skipif(
     sys.platform == "darwin", reason="Only run visualizer test on Linux"
 )
 def test_get_pdf_dim(caplog):
+    from fonduer.utils.visualizer import get_pdf_dim  # noqa
+
     """Test get_pdf_dim on different pages"""
     assert get_pdf_dim("tests/data/pdf/BC546A_Series_B14-521026.pdf") == (729, 1032)
     assert get_pdf_dim("tests/data/pdf/BC546A_Series_B14-521026.pdf", page=1) == (
