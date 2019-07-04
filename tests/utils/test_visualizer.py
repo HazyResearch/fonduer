e95c7c35db8a9da0003f494d09cb03b1769de4c0
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2019-07-04 16:49:33 -0700

    WIP: Add page argument to get_pdf_dim in case pages have different dimensions (#280)
    * Add page argument to get_pdf_dim in case pages have different dimensions
    
    * Skip test_get_pdf_dim on MacOSX
    
    * Import get_pdf_dim only on Linux
    
    * Consolidate unit tests for fonduer.utils.visualizer and rename the file to tests/utils/test_visualizer.py

--- tests/utils/test_visualizer.py
+++ tests/utils/test_visualizer.py
@@ -74,3 +74,22 @@ def test_visualizer(caplog):
     pdf_path = "tests/data/pdf_simple"
     vis = Visualizer(pdf_path)
     vis.display_candidates([cands[0]])
+
+
+# TODO test on MacOSX too
+@pytest.mark.skipif(
+    sys.platform == "darwin", reason="Only run visualizer test on Linux"
+)
+def test_get_pdf_dim(caplog):
+    from fonduer.utils.visualizer import get_pdf_dim  # noqa
+
+    """Test get_pdf_dim on different pages"""
+    assert get_pdf_dim("tests/data/pdf/BC546A_Series_B14-521026.pdf") == (729, 1032)
+    assert get_pdf_dim("tests/data/pdf/BC546A_Series_B14-521026.pdf", page=1) == (
+        729,
+        1032,
+    )
+    assert get_pdf_dim("tests/data/pdf/BC546A_Series_B14-521026.pdf", page=6) == (
+        612,
+        792,
+    )
