5b7fb15134d533eed4b643e1acb73bf767d4770e
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2019-09-09 18:08:49 -0700

    Add types to tensorboard_writer.py

--- src/fonduer/utils/logging/tensorboard_writer.py
+++ src/fonduer/utils/logging/tensorboard_writer.py
@@ -5,13 +5,13 @@ class TensorBoardLogger(object):
     """A class for logging to Tensorboard during training process.
     """
 
-    def __init__(self, log_dir):
+    def __init__(self, log_dir: str):
         """Create a summary writer logging to log_dir."""
         self.writer = SummaryWriter(log_dir)
 
-    def add_scalar(self, name, value, step):
+    def add_scalar(self, name: str, value: float, step: int) -> None:
         """Log a scalar variable."""
         self.writer.add_scalar(name, value, step)
 
-    def close(self):
+    def close(self) -> None:
         self.writer.close()
