a52ca17b208754909818f41b2f3340d77290b70c
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-01-24 16:03:43 -0800

    [test] 'visualizer_test' no longer required for test_visualizer.py

--- tests/utils/test_visualizer.py
+++ tests/utils/test_visualizer.py
@@ -4,17 +4,12 @@ visualizer_test has been created for the purpose of testing.
 
 If you are testing locally, you will need to create this db.
 """
-from fonduer import Meta
-from fonduer.candidates import CandidateExtractor, MentionExtractor, MentionNgrams
+from fonduer.candidates import MentionNgrams
+from fonduer.candidates.candidates import CandidateExtractorUDF
 from fonduer.candidates.matchers import OrganizationMatcher
+from fonduer.candidates.mentions import MentionExtractorUDF
 from fonduer.candidates.models import candidate_subclass, mention_subclass
-from fonduer.parser import Parser
-from fonduer.parser.models import Document
-from fonduer.parser.preprocessors import HTMLDocPreprocessor
-
-DB = "visualizer_test"
-# Use 127.0.0.1 instead of localhost (#351)
-CONN_STRING = f"postgresql://127.0.0.1:5432/{DB}"
+from tests.candidates.test_candidates import parse_doc
 
 
 def test_visualizer():
@@ -22,24 +17,11 @@ def test_visualizer():
 
     """Unit test of visualizer using the md document.
     """
-    session = Meta.init(CONN_STRING).Session()
-
-    PARALLEL = 1
-    max_docs = 1
     docs_path = "tests/data/html_simple/md.html"
     pdf_path = "tests/data/pdf_simple/md.pdf"
 
-    # Preprocessor for the Docs
-    doc_preprocessor = HTMLDocPreprocessor(docs_path, max_docs=max_docs)
-
-    # Create an Parser and parse the md document
-    corpus_parser = Parser(
-        session, structural=True, lingual=True, visual=True, pdf_path=pdf_path
-    )
-    corpus_parser.apply(doc_preprocessor, parallelism=PARALLEL)
-
     # Grab the md document
-    doc = session.query(Document).order_by(Document.name).all()[0]
+    doc = parse_doc(docs_path, "md", pdf_path)
     assert doc.name == "md"
 
     organization_ngrams = MentionNgrams(n_max=1)
@@ -48,19 +30,21 @@ def test_visualizer():
 
     organization_matcher = OrganizationMatcher()
 
-    mention_extractor = MentionExtractor(
-        session, [Org], [organization_ngrams], [organization_matcher]
+    mention_extractor_udf = MentionExtractorUDF(
+        [Org], [organization_ngrams], [organization_matcher]
     )
 
-    mention_extractor.apply([doc], parallelism=PARALLEL)
+    doc = mention_extractor_udf.apply(doc)
 
     Organization = candidate_subclass("Organization", [Org])
 
-    candidate_extractor = CandidateExtractor(session, [Organization])
+    candidate_extractor_udf = CandidateExtractorUDF(
+        [Organization], None, False, False, True
+    )
 
-    candidate_extractor.apply([doc], split=0, parallelism=PARALLEL)
+    doc = candidate_extractor_udf.apply(doc, split=0)
 
-    cands = session.query(Organization).filter(Organization.split == 0).all()
+    cands = doc.organizations
 
     # Test visualizer
     pdf_path = "tests/data/pdf_simple"
