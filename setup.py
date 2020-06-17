120c2d32f27b6eec873ef00caaa1c3e2d9d04e8e
wajdi <wajdikhattel@think-it.io>
2020-06-17 17:47:57 +0100

    chore: update treedlib min version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "sqlalchemy[postgresql]>=1.3.7, <2.0.0",
         "torch>=1.3.1,<2.0.0",
         "tqdm>=4.36.0, <5.0.0",
-        "treedlib>=0.1.1, <0.2.0",
+        "treedlib>=0.1.3, <0.2.0",
         "wand>=0.4.4, <0.6.0",
         "ipython",
     ],
