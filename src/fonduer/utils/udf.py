7bc48bd15307b4a7a69422db3aeee7910670fc0b
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-07-21 09:38:36 -0700

    Update inline comments

--- src/fonduer/utils/udf.py
+++ src/fonduer/utils/udf.py
@@ -129,8 +129,9 @@ class UDFRunner(object):
         for udf in self.udfs:
             udf.start()
 
+        # Fill input queue with documents but # of docs in queue is capped (#435).
         def in_thread_func() -> None:
-            # Fill input queue with documents but # of docs in queue is capped (#435).
+            # Do not use session here to prevent concurrent use (#485).
             for doc in doc_loader:
                 in_queue.put(doc)  # block until a free slot is available
 
