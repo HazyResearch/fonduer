35cc2b6ea217cc9ea50c8028154efacbab2c90b7
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-09-10 16:05:11 -0700

    Register HOCRDocPreprocessor

--- src/fonduer/parser/preprocessors/__init__.py
+++ src/fonduer/parser/preprocessors/__init__.py
@@ -1,6 +1,7 @@
 """Fonduer's parser preprocessor module."""
 from fonduer.parser.preprocessors.csv_doc_preprocessor import CSVDocPreprocessor
 from fonduer.parser.preprocessors.doc_preprocessor import DocPreprocessor
+from fonduer.parser.preprocessors.hocr_doc_preprocessor import HOCRDocPreprocessor
 from fonduer.parser.preprocessors.html_doc_preprocessor import HTMLDocPreprocessor
 from fonduer.parser.preprocessors.text_doc_preprocessor import TextDocPreprocessor
 from fonduer.parser.preprocessors.tsv_doc_preprocessor import TSVDocPreprocessor
@@ -8,6 +9,7 @@ from fonduer.parser.preprocessors.tsv_doc_preprocessor import TSVDocPreprocessor
 __all__ = [
     "CSVDocPreprocessor",
     "DocPreprocessor",
+    "HOCRDocPreprocessor",
     "HTMLDocPreprocessor",
     "TSVDocPreprocessor",
     "TextDocPreprocessor",
