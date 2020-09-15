63f5a68def9c60c723e7b80bef1ae057e1a054c4
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-09-15 09:14:24 -0700

    Fix style check errors

--- src/fonduer/parser/preprocessors/hocr_doc_preprocessor.py
+++ src/fonduer/parser/preprocessors/hocr_doc_preprocessor.py
@@ -1,42 +1,18 @@
-"""Fonduer HTML document preprocessor."""
+"""Fonduer hOCR document preprocessor."""
 import codecs
 import os
 import sys
-from typing import Iterator
+from typing import Iterator, Optional, Tuple
 
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 
 from fonduer.parser.models import Document
 from fonduer.parser.preprocessors.doc_preprocessor import DocPreprocessor
 
 
-# Adapted from https://github.com/ocropus/hocr-tools/blob/v1.3.0/hocr-check#L29-L38
-def get_prop(node, name):
-    title = node["title"]
-    if not title:
-        return None
-    props = title.split(";")
-    for prop in props:
-        (key, args) = prop.split(None, 1)
-        if key == name:
-            return args
-    return None
-
-
-# Adapted from https://github.com/ocropus/hocr-tools/blob/v1.3.0/hocr-check#L41-L45
-def get_bbox(node):
-    bbox = get_prop(node, "bbox")
-    if not bbox:
-        return None
-    return tuple([x for x in bbox.split()])
-
-
 class HOCRDocPreprocessor(DocPreprocessor):
-    """A ``Document`` generator for hOCR files.
-
-    :param space: boolean value indicating whether each word should have a subsequent
-       space. E.g., English sentence has spaces between words. Japanese one does not.
-    """
+    """A ``Document`` generator for hOCR files."""
 
     def __init__(
         self,
@@ -45,10 +21,40 @@ class HOCRDocPreprocessor(DocPreprocessor):
         max_docs: int = sys.maxsize,
         space: bool = True,
     ):
+        """Initialize HOCRDocPreprocessor.
+
+        :param path: a path to file or directory, or a glob pattern. The basename
+            (as returned by ``os.path.basename``) should be unique among all files.
+        :param encoding: file encoding to use, defaults to "utf-8".
+        :param max_docs: the maximum number of ``Documents`` to produce,
+            defaults to sys.maxsize.
+        :param space: boolean value indicating whether each word should have a
+            subsequent space. E.g., English has spaces between words.
+        :return: A generator of ``Documents``.
+        """
         super().__init__(path, encoding, max_docs)
         self.space = space
 
     def _parse_file(self, fp: str, file_name: str) -> Iterator[Document]:
+        # Adapted from https://github.com/ocropus/hocr-tools/blob/v1.3.0/hocr-check
+        def get_prop(node: Tag, name: str) -> Optional[str]:
+            title = node["title"]
+            if not title:
+                return None
+            props = title.split(";")
+            for prop in props:
+                (key, args) = prop.split(None, 1)
+                if key == name:
+                    return args
+            return None
+
+        # Adapted from https://github.com/ocropus/hocr-tools/blob/v1.3.0/hocr-check
+        def get_bbox(node: Tag) -> Tuple[str, ...]:
+            bbox = get_prop(node, "bbox")
+            if not bbox:
+                return None
+            return tuple([x for x in bbox.split()])
+
         with codecs.open(fp, encoding=self.encoding) as f:
             soup = BeautifulSoup(f, "lxml")
             all_html_elements = soup.find_all("html")
--- tests/parser/test_preprocessor.py
+++ tests/parser/test_preprocessor.py
@@ -1,7 +1,10 @@
+"""Unit tests for preprocessors."""
+
 from fonduer.parser.preprocessors.hocr_doc_preprocessor import HOCRDocPreprocessor
 
 
 def test_hocrpreprocessor():
+    """Test hOCRDocPreprocessor."""
     path = "tests/data/hocr_simple/md.hocr"
     preprocessor = HOCRDocPreprocessor(path=path)
     doc = next(preprocessor.__iter__())
