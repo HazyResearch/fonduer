2643fd753c3b4e036e1627b42eab404b33ca9234
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-09-25 15:34:28 -0700

    Update docs

--- src/fonduer/parser/visual_parser/visual_parser.py
+++ src/fonduer/parser/visual_parser/visual_parser.py
@@ -14,8 +14,19 @@ class VisualParser(ABC):
         document_name: str,
         sentences: Iterable[Sentence],
     ) -> Iterator[Sentence]:
+        """Parse visual information and link them with given sentences.
+
+        :param document_name: the document name.
+        :param sentences: sentences to be linked with visual information.
+        :yield: sentences with visual information.
+        """
         pass
 
     @abstractmethod
     def is_parsable(self, document_name: str) -> bool:
+        """Check if visual information can be parsed.
+
+        :param document_name: the document name.
+        :return: Whether visual information is parsable.
+        """
         pass
